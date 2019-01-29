feb310e131ffce3a75c790fedf8bb23ff2fc3a0d
Brandon L. Borrero <borrero509@outlook.com>
2019-01-29 14:19:56 -0800

    1 more event and used prevent propagation

--- js/index.js
+++ js/index.js
@@ -9,10 +9,19 @@ const body = document.querySelector('body');
 body.addEventListener('keydown', e => {
     body.style.zoom = "120%";
 });
-
 body.addEventListener('keyup', e => {
     body.style.zoom = "100%";
 });
 
- 
+const header = document.querySelector('.main-navigation');
+header.addEventListener('click', e => {
+    header.style.backgroundColor = "wheat";
+});
+
+const title = document.querySelector('.logo-heading');
+title.addEventListener('click', e => {
+    title.style.color = "skyblue";
+    e.stopPropagation();
+});
+
 
