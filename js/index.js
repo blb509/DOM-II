f8f4230712b81ca6fa1c3df38dc04c56c6e5cd16
Brandon L. Borrero <borrero509@outlook.com>
2019-01-29 13:40:54 -0800

    3 events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+const nav = document.querySelector('nav');
+nav.addEventListener('mouseover', e => {
+    nav.style.justifyContent = "space-between";
+});
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', e => {
+    body.style.zoom = "120%";
+});
+
+body.addEventListener('keyup', e => {
+    body.style.zoom = "100%";
+});
+
+ 
+
